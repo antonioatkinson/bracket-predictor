3e7fcd7c0ce675a6168d75c530c8fa678b48a9ba
Antonio Thomas Atkinson <Antonio01pd2014@atkinson-antonio-thomas-2.local>
2017-08-05 12:24:20 -0400

    Initial commit

---
+++ Bracket.h
@@ -0,0 +1,131 @@
+//
+//  Bracket.h
+//  March Madness Bracket Predictor
+//
+//  Created by Antonio Thomas Atkinson on 6/4/17.
+//  Copyright © 2017 Antonio Thomas Atkinson. All rights reserved.
+//
+
+#ifndef Bracket_h
+#define Bracket_h
+#include <string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+enum Regions{
+    East,
+    West,
+    Midwest,
+    South
+};
+struct TeamInfo{
+    string name;
+    int seed;
+    //string region;//Make enum of regions
+    string regionName; //Might not be needed
+    Regions region;
+    double RPI;
+    int RPIRank;
+    double BPI;
+    int BPIRank;
+    double kenpom;
+    double kenpomRank;
+    double sagarin;
+    int sagarinRank;
+    string initialOpponent;
+    int matchupNumber;//Where matchup is located in bracket 
+    //Cacluations
+    double avgRankWeight;
+    double avgScoreWeight;
+    double medianRankWeight;
+};
+
+class Bracket{
+public:
+    Bracket(vector<TeamInfo>&vec_in,unordered_map<string,int> &teamIndex_in):vec(vec_in),teamIndex(teamIndex_in){
+        vecRoundof64.resize(32);
+        /*for(int i=0;i<32;++i){
+            vecRoundof64[i].resize(2);
+        }*/
+        vecRoundof32.resize(16);
+        vecSweetSixteen.resize(16);
+        vecEliteEight.resize(8);
+        vecFinalFour.resize(4);
+        vecNationalChampionship.resize(2);
+    }
+    void RunFirstRound(){
+        for(int i=0;i<(int)vecRoundof64.size();++i){
+            vecRoundof64[vec[i].matchupNumber].push_back(&vec[i]);
+        }
+        printRound(vecRoundof64);
+        if(vecRoundof64[8][0]->avgRankWeight<vecRoundof64[8][1]->avgRankWeight){
+            cout<<"Winner: "<<vecRoundof64[8][0]->name<<endl;
+            cout<<"Winner: "<<vecRoundof64[8][0]->avgRankWeight<<endl;
+            cout<<"Winner: "<<vecRoundof64[8][0]->avgScoreWeight<<endl;
+            cout<<endl;
+            cout<<"Winner: "<<vecRoundof64[8][1]->name<<endl;
+            cout<<"Winner: "<<vecRoundof64[8][1]->avgRankWeight<<endl;
+            cout<<"Winner: "<<vecRoundof64[8][1]->avgScoreWeight<<endl;
+            
+            
+            cout<<"new round num: "<<vecRoundof64[8][0]->matchupNumber/2<<endl;
+            vec[teamIndex[vecRoundof64[8][0]->name]].matchupNumber=vecRoundof64[8][0]->matchupNumber/2;
+        }
+        else
+            cout<<"Winner: "<<vecRoundof64[0][1]->name;
+        
+        
+        cout<<vecRoundof64[0][0]->name<<endl;
+        cout<<vecRoundof64[0][1]->name<<endl;
+        cout<<vecRoundof64[8][0]->name<<endl;
+        cout<<vecRoundof64[8][1]->name<<endl;
+        cout<<vecRoundof64[16][0]->name<<endl;
+        cout<<vecRoundof64[16][1]->name<<endl;
+        cout<<vecRoundof64[24][0]->name<<endl;
+        cout<<vecRoundof64[24][1]->name<<endl;
+    }
+    void RunSecondRound();
+    void RunSweetSixteen();
+    void RunEliteEight();
+    void RunFinalFour();
+    void RunNationalChampionship();
+private:
+    vector<TeamInfo>vec;
+    //For later rounds
+    vector<vector<TeamInfo*>> vecRoundof64;
+    vector<vector<TeamInfo*>> vecRoundof32;
+    vector<TeamInfo> vecSweetSixteen;
+    vector<TeamInfo> vecEliteEight;
+    vector<TeamInfo> vecFinalFour;
+    vector<TeamInfo> vecNationalChampionship;
+    TeamInfo nationChampion;
+    unordered_map<string,int> teamIndex;
+    
+    //Helper functions
+    void calcRound(vector<TeamInfo> &vec){
+        for(int i=0;i<(int)vec.size();++i){
+            
+        }
+    }
+    void printRound(vector<vector<TeamInfo*>>&vec1){
+        for(int i=0;i<(int)vec1.size()*2;++i){
+            if(i%8==0&&i!=0){
+                cout<<endl<<endl;
+            }
+            
+            if(i==0||i==8||i==16||i==24){
+                cout<<vec1[i][0]->name<<endl<<vec1[i][1]->name<<endl<<endl;
+            }
+            else{
+                cout<<"------"<<endl<<"----"<<endl<<endl;
+            }
+        }
+    }
+    
+};
+
+
+#endif /* Bracket_h */
---
+++ main.cpp
@@ -0,0 +1,125 @@
+//
+//  main.cpp
+//  March Madness Bracket Predictor
+//
+//  Created by Antonio Thomas Atkinson on 6/3/17.
+//  Copyright © 2017 Antonio Thomas Atkinson. All rights reserved.
+//
+
+#include <iostream>
+#include <unordered_map>
+#include "Bracket.h"
+#include <vector>
+#include <string>
+#include <getopt.h>
+using namespace std;
+
+void readData(vector<TeamInfo> &vec,unordered_map<string,int> &teamIndex);
+void calcInitialData(vector<TeamInfo> &vec,unordered_map<string, int> &teamIndex);
+void determineRegion(vector<TeamInfo> &vec,char region,int i);
+void runProgram(vector<TeamInfo> &vec,unordered_map<string, int> &teamIndex);
+void testPrint(vector<TeamInfo> &vec,unordered_map<string,int> &teamIndex);
+
+int main(int argc, char * argv[]) {
+    ios_base::sync_with_stdio(false);
+    if (getenv("INPUT")) {         freopen(getenv("INPUT"), "r", stdin);   freopen(getenv("OUTPUT"), "w", stdout);     }
+    
+    /*opterr = true; // Give us help with errors
+    
+    static struct option longOpts[] =
+    {
+        {"stack",no_argument,nullptr,'s'},
+        {"help", no_argument, nullptr, 'h'},
+        {nullptr, 0, nullptr, 0}
+    };
+    
+    int idx = 0; // getopt_long stores the option index here
+    int c;       // The single character for the option, but stored as int
+    //string dArg;
+    while ((c = getopt_long(argc, argv, "sqcplo:b:e:h", longOpts, &idx))!=-1)
+    {
+        switch (c) {
+            case 's':
+                break;
+            
+            case 'h':
+                cout << "-s --stack: Stack based routing scheme\n";
+                cout << "-q --queue: Queue based routing scheme\n";
+                cout << "-c --change: Letterman Allowed to change one letter to another\n";
+                cout << "-p --swap: Letterman allowed to swap two adjacent characters\n";
+                cout << "-l --length: Allowed to modify length of word by adding or deleting a letter\n";
+                cout << "-o(W|M) --output(W|M): Specifies output\n";
+                cout << "-b --begin: Beginning word\n";
+                cout << "-e --end: Ending word\n";
+                cout << "-h --help: Help\n";
+                c1.help = true;
+                exit(0);
+            default:
+                cerr << "Unknown option " << c << endl;
+                exit(1);
+        }
+    }*/
+    
+    int numOfTeams=0;
+    cin>>numOfTeams;
+    vector<TeamInfo> vec;
+    vec.resize(numOfTeams);
+    
+    unordered_map<string, int> teamIndex;
+    
+    string input;
+    cin>>input>>input>>input>>input>>input>>input>>input>>input>>input>>input>>input>>input>>input;
+    
+    readData(vec, teamIndex);
+    calcInitialData(vec, teamIndex); 
+    runProgram(vec, teamIndex);
+    //testPrint(vec, teamIndex);
+    
+    return 0;
+}
+void readData(vector<TeamInfo> &vec,unordered_map<string, int> &teamIndex){
+    string region;
+    for(int i=0;i<(int)vec.size();++i){
+        cin>>vec[i].name>>vec[i].seed>>region;//Could make more efficient or remove completly 
+        vec[i].regionName=region;
+        determineRegion(vec, region[0],i);
+        
+        cin>>vec[i].RPI>>vec[i].RPIRank>>vec[i].BPI>>vec[i].BPIRank>>vec[i].kenpom>>vec[i].kenpomRank>>vec[i].sagarin>>vec[i].sagarinRank>>vec[i].initialOpponent>>vec[i].matchupNumber;
+        
+        teamIndex[vec[i].name] = i;
+    }
+}
+void calcInitialData(vector<TeamInfo> &vec,unordered_map<string, int> &teamIndex){
+    for(int i=0;i<(int)vec.size();++i){
+        vec[i].avgRankWeight = (1*vec[i].sagarinRank+2*vec[i].kenpomRank+3*vec[i].BPIRank+4*vec[i].RPIRank)/10;
+        vec[i].avgScoreWeight = (4*vec[i].sagarin+3*vec[i].kenpom+2*vec[i].BPI+1*vec[i].RPI)/10;
+    }
+}
+void runProgram(vector<TeamInfo> &vec,unordered_map<string, int> &teamIndex){
+    Bracket bracket(vec,teamIndex);
+    bracket.RunFirstRound();
+}
+void determineRegion(vector<TeamInfo> &vec,char region,int i){
+    switch (region) {
+        case 'W':
+            vec[i].region=West;
+            break;
+        case 'E':
+            vec[i].region=East;
+            break;
+        case 'M':
+            vec[i].region=Midwest;
+            break;
+        case 'S':
+            vec[i].region=South;
+            break;
+        default:
+            break;
+    }
+}
+void testPrint(vector<TeamInfo> &vec,unordered_map<string,int> &teamIndex){
+    for(int i=0;i<(int)vec.size();++i){
+        cout<<vec[i].name<<" "<<vec[i].seed<<" "<<vec[i].region<<" "<<vec[i].RPI<<" "<<vec[i].RPIRank<<" "<<vec[i].BPI<<" "<<
+        vec[i].BPIRank<<" "<<vec[i].kenpom<<" "<<vec[i].kenpomRank<<" "<<vec[i].sagarin<<" "<<vec[i].sagarinRank<<" "<<vec[i].initialOpponent<<" "<<vec[i].matchupNumber<<"\n";
+    }
+}
